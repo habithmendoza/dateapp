971539075eabc2058a2bbf512eb35344af143712
habimendoza <habimendoza@hotmail.com>
2019-12-23 17:49:04 -0500

    Updates

---
+++ precache-manifest.aa2c6812519fd4b3750bdbe96344654b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f969d1138b630d36a9d5a2f61a528f14",
+    "url": "/dateapp/index.html"
+  },
+  {
+    "revision": "b9d898c049dec8a2fdc9",
+    "url": "/dateapp/static/css/main.b11a18bc.chunk.css"
+  },
+  {
+    "revision": "116778e6b88e8b22ab32",
+    "url": "/dateapp/static/js/2.7a60793b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dateapp/static/js/2.7a60793b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9d898c049dec8a2fdc9",
+    "url": "/dateapp/static/js/main.1b68ca64.chunk.js"
+  },
+  {
+    "revision": "07ea3282f11273a58619",
+    "url": "/dateapp/static/js/runtime-main.6838f17c.js"
+  },
+  {
+    "revision": "19ba7aa52a78c3896558ac1c0a5fb4c7",
+    "url": "/dateapp/static/media/Montserrat-ExtraBold.19ba7aa5.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/dateapp/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/dateapp/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/dateapp/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
